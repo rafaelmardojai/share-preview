99fd626b36496cddf82cfaece827a4018b6a064c
Rafael Mardojai CM <mardojai.cardenas@gmail.com>
2021-04-15 11:26:10 -0500

    Init with GTK Rust Template

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+mod application;
+#[rustfmt::skip]
+mod config;
+mod window;
+
+use application::ExampleApplication;
+use config::{GETTEXT_PACKAGE, LOCALEDIR, RESOURCES_FILE};
+use gettextrs::*;
+use gtk::gio;
+
+fn main() {
+    // Initialize logger, debug is carried out via debug!, info!, and warn!.
+    pretty_env_logger::init();
+
+    // Prepare i18n
+    setlocale(LocaleCategory::LcAll, "");
+    bindtextdomain(GETTEXT_PACKAGE, LOCALEDIR);
+    textdomain(GETTEXT_PACKAGE);
+
+    gtk::glib::set_application_name("Share Preview");
+    gtk::glib::set_prgname(Some("share-preview"));
+
+    gtk::init().expect("Unable to start GTK4");
+
+    let res = gio::Resource::load(RESOURCES_FILE).expect("Could not load gresource file");
+    gio::resources_register(&res);
+
+    let app = ExampleApplication::new();
+    app.run();
+}
---
+++ src/window.rs
@@ -0,0 +1,119 @@
+use crate::application::ExampleApplication;
+use crate::config::{APP_ID, PROFILE};
+use glib::signal::Inhibit;
+use gtk::subclass::prelude::*;
+use gtk::{self, prelude::*};
+use gtk::{gio, glib, CompositeTemplate};
+use log::warn;
+
+mod imp {
+    use super::*;
+
+    #[derive(Debug, CompositeTemplate)]
+    #[template(resource = "/com/rafaelmardojai/SharePreview/window.ui")]
+    pub struct ExampleApplicationWindow {
+        #[template_child]
+        pub headerbar: TemplateChild<gtk::HeaderBar>,
+        pub settings: gio::Settings,
+    }
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for ExampleApplicationWindow {
+        const NAME: &'static str = "ExampleApplicationWindow";
+        type Type = super::ExampleApplicationWindow;
+        type ParentType = gtk::ApplicationWindow;
+
+        fn new() -> Self {
+            Self {
+                headerbar: TemplateChild::default(),
+                settings: gio::Settings::new(APP_ID),
+            }
+        }
+
+        fn class_init(klass: &mut Self::Class) {
+            Self::bind_template(klass);
+        }
+
+        // You must call `Widget`'s `init_template()` within `instance_init()`.
+        fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
+            obj.init_template();
+        }
+    }
+
+    impl ObjectImpl for ExampleApplicationWindow {
+        fn constructed(&self, obj: &Self::Type) {
+            self.parent_constructed(obj);
+
+            let builder =
+                gtk::Builder::from_resource("/com/rafaelmardojai/SharePreview/shortcuts.ui");
+            let shortcuts = builder.get_object("shortcuts").unwrap();
+            obj.set_help_overlay(Some(&shortcuts));
+
+            // Devel Profile
+            if PROFILE == "Devel" {
+                obj.get_style_context().add_class("devel");
+            }
+
+            // load latest window state
+            obj.load_window_size();
+        }
+    }
+
+    impl WidgetImpl for ExampleApplicationWindow {}
+    impl WindowImpl for ExampleApplicationWindow {
+        // save window state on delete event
+        fn close_request(&self, obj: &Self::Type) -> Inhibit {
+            if let Err(err) = obj.save_window_size() {
+                warn!("Failed to save window state, {}", &err);
+            }
+            Inhibit(false)
+        }
+    }
+
+    impl ApplicationWindowImpl for ExampleApplicationWindow {}
+}
+
+glib::wrapper! {
+    pub struct ExampleApplicationWindow(ObjectSubclass<imp::ExampleApplicationWindow>)
+        @extends gtk::Widget, gtk::Window, gtk::ApplicationWindow, @implements gio::ActionMap, gio::ActionGroup;
+}
+
+impl ExampleApplicationWindow {
+    pub fn new(app: &ExampleApplication) -> Self {
+        let window: Self =
+            glib::Object::new(&[]).expect("Failed to create ExampleApplicationWindow");
+        window.set_application(Some(app));
+
+        // Set icons for shell
+        gtk::Window::set_default_icon_name(APP_ID);
+
+        window
+    }
+
+    pub fn save_window_size(&self) -> Result<(), glib::BoolError> {
+        let settings = &imp::ExampleApplicationWindow::from_instance(self).settings;
+
+        let size = self.get_default_size();
+
+        settings.set_int("window-width", size.0)?;
+        settings.set_int("window-height", size.1)?;
+
+        settings.set_boolean("is-maximized", self.is_maximized())?;
+
+        Ok(())
+    }
+
+    fn load_window_size(&self) {
+        let settings = &imp::ExampleApplicationWindow::from_instance(self).settings;
+
+        let width = settings.get_int("window-width");
+        let height = settings.get_int("window-height");
+        let is_maximized = settings.get_boolean("is-maximized");
+
+        self.set_default_size(width, height);
+
+        if is_maximized {
+            self.maximize();
+        }
+    }
+}
