c889f5324cf67e552d07ac1a0a9bbc05f9784a69
Rafael Mardojai CM <mardojai.cardenas@gmail.com>
2021-04-29 19:13:04 -0500

    Backend: Implement images fetching

--- src/backend/image.rs
+++ src/backend/image.rs
@@ -1,6 +1,8 @@
 // Copyright 2021 Rafael Mardojai CM
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+use super::{Error, CLIENT};
+use gtk::{gio, glib, gdk_pixbuf};
 use url::{Url, ParseError};
 
 #[derive(Debug, Clone)]
@@ -12,6 +14,7 @@ impl Image {
     pub fn new(url: String) -> Image {
         Image {url}
     }
+    
     pub fn normalize(&mut self, url: &Url) -> &mut Image {
         if let Err(e) = Url::parse(&self.url) {
             match e {
@@ -24,5 +27,14 @@ impl Image {
             }
         }
         self
+    }
+
+    pub async fn fetch(&self) -> Result<gdk_pixbuf::Pixbuf, Error> {
+        let mut resp = CLIENT.get(&self.url).await?;
+        let bytes = resp.body_bytes().await?;
+        let bytes = glib::Bytes::from(&bytes);
+        let stream = gio::MemoryInputStream::from_bytes(&bytes);
+
+        Ok(gdk_pixbuf::Pixbuf::from_stream(&stream, gio::NONE_CANCELLABLE).unwrap())
     }
 }
